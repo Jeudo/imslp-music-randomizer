7a5880ba35ab66c99d4690b4bc24dd0f9300b056
Justus Rodriguez <justusroddy@gmail.com>
2021-07-24 13:17:25 -0700

    Consolidate the tutorial messages
    This commit created a function that prints a sequence of messages,
    originally designed to be in interface.py, as an option for a tutorial
    to make code cleaner on interface.py

--- functions.py
+++ functions.py
@@ -11,6 +11,21 @@ def clickShuffle():
 
 
 
+def tutorial():
+  print('This program seeks to streamline the process of finding new classical music pieces.\n')
+  time.sleep(1.5)
+  print('This program will accomplish its goal by using the International Music Score Library Project (IMSLP) database of composers and compositions.\n')
+  time.sleep(3)
+  print('You will be provided with three tiers of options to find a piece of classical music:')
+  time.sleep(2)
+  print('1) a completely random composer from the entire IMSLP database')
+  print('2) a random or chosen popular musical era')
+  print('3) a random or chosen popular composer from the established era\n') 
+  time.sleep(3)
+  print('If you find a random era/composer or chosen era/composer to be unfit to your liking, then you may choose to restart, OR keep running the program to produce new results.\n')
+  time.sleep(3)
+
+
 
 # exProtocol
  
