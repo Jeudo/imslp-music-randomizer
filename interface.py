96b477c667c65975d4fa822a357388c204db912b
Justus Rodriguez <justusroddy@gmail.com>
2021-07-24 13:19:00 -0700

    Remove the unnecessary print statements
    This commit removed the introductory print statements
    (from interface.py) that now reside in the tutorial function.

--- interface.py
+++ interface.py
@@ -7,18 +7,6 @@ from functions import *
 
 print('Welcome to the composer shuffler!\n')
 time.sleep(1.5)
-print('This program seeks to streamline the process of finding new classical music pieces.\n')
-time.sleep(1.5)
-print('This program will accomplish its goal by using the International Music Score Library Project (IMSLP) database of composers and compositions.\n')
-time.sleep(3)
-print('You will be provided with three tiers of options to find a piece of classical music:')
-time.sleep(2)
-print('1) a completely random composer from the entire IMSLP database')
-print('2) a random or chosen popular musical era')
-print('3) a random or chosen popular composer from the established era\n') 
-time.sleep(3)
-print('If you find a random era/composer or chosen era/composer to be unfit to your liking, then you may choose to restart, OR keep running the program to produce new results.\n')
-time.sleep(3)
 print('Would you like a random composer, or to continue on to the musical eras?\n')
 time.sleep(1.5)
  
