d471ea06800f674c130e58012131ae5fa7c9c6b5
Justus Rodriguez <justusroddy@gmail.com>
2021-07-24 12:27:00 -0700

    Implement quality-of-life program initiation
    This commit added a brief introduction to the program and explained
    the most basic functionalities, namely what the user would do
    to work the program.

--- interface.py
+++ interface.py
@@ -5,5 +5,9 @@ import time
 from composers import *
 from functions import *
 
-
-
+print('Welcome to the composer shuffler!')
+time.sleep(1.5)
+print('This program provides a collection of musical eras and their respective popular composers to pick from')
+time.sleep(2)
+print('If you find a random era/composer or chosen era/composer to be unfit to your liking, then you may choose to restart, OR keep running the program to produce new results.')
+time.sleep(3)
